901fd7650a515f5fe87c8ceb5ef592d904e22492
mehmetakgul <mehmetakgul.dev@gmail.com>
2022-08-21 14:34:37 +0300

    React State System Which hemisphere are you in?

---
+++ src/HemisphereDisplay.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HemisphereDisplay = () => {
+    return (
+        <div>
+            Hey you are in Npthern Hemisphere!
+        </div>
+    )
+}
+
+export default HemisphereDisplay
--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            latitude: null,
+            longitude: null,
+            errorMessage: '',
+        }
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({
+                    latitude: "enlem (latitude): " + position.coords.latitude,
+                    longitude: "boylam (longitude) : " + position.coords.longitude,
+                })
+            },
+            (error) => {
+                this.setState({
+                    errorMessage: error.message,
+                })
+            },
+        );
+    }
+
+    render() {
+        if (this.state.errorMessage && !this.state.latitude) {
+            return <div>{this.state.errorMessage}</div>
+        }
+        if (!this.state.errorMessage && this.state.latitude) {
+            return <div>{this.state.latitude}</div>
+        } else {
+            return <div>Loading...</div>
+        }
+    }
+}
+
+ReactDom.render(
+    <App/>,
+    document.querySelector('#root')
+)
