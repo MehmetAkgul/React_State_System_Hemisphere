5edc9dd9245f3525292ee25969375799818fac08
mehmetakgul <mehmetakgul.dev@gmail.com>
2022-08-21 15:18:47 +0300

    React Life Cycle show picture

--- src/HemisphereDisplay.js
+++ src/HemisphereDisplay.js
@@ -1,11 +1,14 @@
 import React from "react";
+import northernPic from './image/northern.png';
+import southernPic from './image/southern.jpeg';
 
 const HemisphereDisplay = ({latitude}) => {
     console.log(latitude)
     const hemisphere = latitude > 0 ? 'Northern hemisphere' : 'Southern hemisphere';
+    const picture = latitude > 0 ? northernPic : southernPic;
     return (
         <div>
-            {hemisphere}
+            <img src={picture} alt={hemisphere}/>
         </div>
     )
 }
