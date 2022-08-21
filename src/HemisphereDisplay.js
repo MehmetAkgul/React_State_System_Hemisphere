dacc39ff9c0a342bff15bc0c6850a18a2226bf4c
mehmetakgul <mehmetakgul.dev@gmail.com>
2022-08-21 15:23:12 +0300

    React Life Cycle component configuration

--- src/HemisphereDisplay.js
+++ src/HemisphereDisplay.js
@@ -2,13 +2,24 @@ import React from "react";
 import northernPic from './image/northern.png';
 import southernPic from './image/southern.jpeg';
 
+const hemisphereConfig = {
+    Northern: {
+        text: "it is northern hemisphere",
+        picture: northernPic,
+    },
+    Southern: {
+        text: "it is southern hemisphere",
+        picture: southernPic,
+    },
+}
+
 const HemisphereDisplay = ({latitude}) => {
     console.log(latitude)
-    const hemisphere = latitude > 0 ? 'Northern hemisphere' : 'Southern hemisphere';
-    const picture = latitude > 0 ? northernPic : southernPic;
+    const hemisphere = latitude > 0 ? 'Northern' : 'Southern';
+    const {text, picture} = hemisphereConfig[hemisphere];
     return (
         <div>
-            <img src={picture} alt={hemisphere}/>
+            <img src={picture} alt={text}/>
         </div>
     )
 }
